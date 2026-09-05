284b0a147b5fd911130b68b69e28566aeae35482
afrose0 <afrosetarin0@gmail.com>
2026-09-06 00:28:55 +0600

    lab 4 completed

--- Lab Assignment 4/src/main.cpp
+++ Lab Assignment 4/src/main.cpp
@@ -10,6 +10,26 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+    
+const char *fragmentShaderSource1 = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(0.0f, 1.0f, 1.0f, 1.0f);\n"
+    "}\n\0";
+const char *fragmentShaderSource2 = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.0f, 1.0f, 1.0f);\n"
+    "}\n\0";
+
 int main()
 {
     // glfw: initialize and configure
@@ -17,7 +37,7 @@ int main()
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -25,18 +45,15 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Mahbuba Afrose Tarin", NULL, NULL); //Resizable or not, multiple window sharing?
-
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Mahbuba Afrose Tarin", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-
-    glfwMakeContextCurrent(window); //following all commands on this object, must call this function after glfwCreateWindow() before issuing any OpenGL commands.
-
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); //update when the user stretches the window
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
@@ -44,47 +61,172 @@ int main()
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
-    }    
+    }
+
+
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader1 = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader1, 1, &fragmentShaderSource1, NULL);
+    glCompileShader(fragmentShader1);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader1, GL_COMPILE_STATUS, &success);
+
+    unsigned int fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, NULL);
+    glCompileShader(fragmentShader2);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader2, GL_COMPILE_STATUS, &success);
+
+
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader1, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        glGetShaderInfoLog(fragmentShader2, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // link shaders
+    unsigned int shaderProgram1 = glCreateProgram();
+    glAttachShader(shaderProgram1 , vertexShader);
+    glAttachShader(shaderProgram1 , fragmentShader1);
+    glLinkProgram(shaderProgram1 );
+    
+    // check for linking errors
+    glGetProgramiv(shaderProgram1 , GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram1 , 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader1);
+    unsigned int shaderProgram2 = glCreateProgram();
+    glAttachShader(shaderProgram2, vertexShader);
+    glAttachShader(shaderProgram2, fragmentShader2);
+    glLinkProgram(shaderProgram2);
+    
+    // check for linking errors
+    glGetProgramiv(shaderProgram2, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram2, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader2);
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    
+    float firstTringle[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, 0.5f, 0.0f,  // bottom left
+        
+    };
+    float secondTringle[] = {
+         0.5f,  -0.5f, 0.0f,  // top right
+        - 0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, 0.5f, 0.0f,  // bottom left
+       
+    };
+    float thirdTringle[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+        - 0.5f, 0.5f, 0.0f,  // bottom right
+        0.0f, 1.0f, 0.0f,  // bottom left
+        
+    };
+
+    unsigned int VBO[3], VAO[3];
+    glGenVertexArrays(3, VAO);
+    glGenBuffers(3, VBO);
+    
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(firstTringle), firstTringle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+     glBindVertexArray(VAO[1]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(secondTringle), secondTringle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(VAO[2]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(thirdTringle), thirdTringle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0); 
+
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // render loop
     // -----------
-    //This block of code is the main loop that runs continuously until the window is closed.
-    while (!glfwWindowShouldClose(window)) //checks whether the user has clicked the close button on the window or triggered any event to close it.
+    while (!glfwWindowShouldClose(window))
     {
         // input
         // -----
-        processInput(window); //handle keyboard or mouse input.
-        // changing the window color here using this two lines. mahbuba
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f); //Background golor
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        // Cyan Square
-        glBegin(GL_QUADS);
 
-        glColor3f(0.0f, 1.0f, 1.0f);
+        // draw our first triangle
+        glUseProgram(shaderProgram1);
+        glBindVertexArray(VAO[0]); 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(VAO[1]); 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+           
+        glUseProgram(shaderProgram2);
+        glBindVertexArray(VAO[2]); 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+           
 
-        glVertex2f(-0.2f, 0.2f);
-        glVertex2f(0.2f, 0.2f);
-        glVertex2f(0.2f, -0.2f);
-        glVertex2f(-0.2f, -0.2f);
+        // glDrawElements(GL_TRIANGLES, 0, GL_UNSIGNED_INT, 0);
+        // glBindVertexArray(0); // no need to unbind it every time 
+ 
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)make
 
-        glEnd();
-        // Magenta Equilateral Triangle
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.0f, 0.0f, 1.0f);
-        glVertex2f(-0.2f, 0.2f);
-        glVertex2f(0.2f, 0.2f);
-        glVertex2f(0.0f, 0.5464f);
-
-        glEnd();
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window); //This swaps the back buffer (where your latest OpenGL drawing happens) with the front buffer (the one displayed on screen).
-        //Imagine you're drawing a picture. You draw it on a piece of paper that no one can see, and once you're done, you quickly swap it with the paper on the table that everyone is looking at. The result is a smooth transition, with no one seeing the process of you drawing.
-
-        glfwPollEvents(); //respond to user actions like pressing a key, moving the mouse, or resizing the window.
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
+    // optional: de-allocate all resources once they've outlived their purpose:
+   
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
     glfwTerminate();
@@ -93,9 +235,9 @@ int main()
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window) 
+void processInput(GLFWwindow *window)
 {
-    if(glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS)    //close the window when the user pressed M
+    if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
@@ -104,6 +246,6 @@ void processInput(GLFWwindow *window)
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays. 
+    // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
